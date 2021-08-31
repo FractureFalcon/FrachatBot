0a0ec04e8e2ebd9bdfe632d5ef320ee74ffcbdb7
Fracture <14284627+FractureFalcon@users.noreply.github.com>
2021-08-31 03:36:59 -0700

    Add extension class ControlExtensions with function InvokeWithRequired to encapsulate if InvokeRequired logic

---
+++ FrachatBot/ControlExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrachatBot
+{
+    public static class ControlExtensions
+    {
+        public static void InvokeWithRequiredCheck(this Control control, Action action)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(action);
+            }
+            else
+            {
+                action.Invoke();
+            }
+        }
+    }
+}
